c4c4bde7159294586cca972d7f1fba48581fc6e1
Papa Ray <disciple.ray@icloud.com>
2017-09-20 11:16:53 +0800

    Initial Commit

---
+++ Tap Me/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tap Me
+//
+//  Created by Papa Ray on 9/20/17.
+//  Copyright © 2017 RTG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tap Me/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tap Me
+//
+//  Created by Papa Ray on 9/20/17.
+//  Copyright © 2017 RTG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
